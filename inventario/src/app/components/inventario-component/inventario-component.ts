8acbd867fff56c967c8ecd31d0732e61cfa13a68
AndresMorquecho <morquechoisrael@hotmail.com>
2025-08-16 15:41:36 -0500

    Integracion de servicios REST API

--- inventario/src/app/components/inventario-component/inventario-component.ts
+++ inventario/src/app/components/inventario-component/inventario-component.ts
@@ -3,8 +3,9 @@ import { sucursalModel } from '../../models/sucursalModel';
 import { stockSucursalModel } from '../../models/stockSucursalModel';
 import { ProductoServices } from '../../services/producto-services';
 import { empresaModel } from '../../models/empresaModel';
-import { EmpresaComponent } from "../empresa-component/empresa-component";
-import { ItemInventarioComponent } from "../item-inventario-component/item-inventario-component";
+import { EmpresaComponent } from '../empresa-component/empresa-component';
+import { ItemInventarioComponent } from '../item-inventario-component/item-inventario-component';
+import { dataEmpresaP } from '../../../data/dataEmpresa';
 
 @Component({
   selector: 'app-inventario-component',
@@ -17,7 +18,22 @@ export class InventarioComponent implements OnInit {
 
   constructor(private services: ProductoServices) {}
   ngOnInit(): void {
-    this.stockSucursales = this.services.getStockSucursal();
-    this.datosEmpresa = this.services.getEmpresa();
+    this.services.getEmpresa().subscribe({
+      next: (value) => {
+        this.datosEmpresa = value;
+      },
+      error: (err) => {
+        console.log('error', err);
+      },
+    });
+
+    this.services.getStockSucursal().subscribe({
+      next: (value) => {
+        this.stockSucursales = value;
+      },
+      error: (err) => {
+        console.log('error', err);
+      },
+    });
   }
 }
--- inventario/src/app/services/producto-services.ts
+++ inventario/src/app/services/producto-services.ts
@@ -3,20 +3,29 @@ import { stockSucursalModel } from '../models/stockSucursalModel';
 import { dataSucursal } from '../../data/dataSucursal';
 import { empresaModel } from '../models/empresaModel';
 import { dataEmpresaP } from '../../data/dataEmpresa';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class ProductoServices {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+  private ApiUrl: string = 'http://localhost:3000/api';
   stockProductos: stockSucursalModel[] = dataSucursal;
   dataEmpresa: empresaModel = dataEmpresaP;
 
-  getStockSucursal(): stockSucursalModel[] {
-    return this.stockProductos;
+  getStockSucursal(): Observable<stockSucursalModel[]> {
+    return this.http.get<stockSucursalModel[]>(`${this.ApiUrl}/products`);
+  }
+  getEmpresa(): Observable<empresaModel> {
+    return this.http.get<empresaModel>(`${this.ApiUrl}/company`);
   }
 
-  getEmpresa(): empresaModel {
-    return this.dataEmpresa;
-  }
+  // getStockSucursal(): stockSucursalModel[] {
+  //   return this.stockProductos;
+  // }
+
+  // getEmpresa(): empresaModel {
+  //   return this.dataEmpresa;
+  // }
 }
